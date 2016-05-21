ee8a561402ed305a950629cbd65f723e9cdd9e7a
Matt Rossetta <archanglmr@yahoo.com>
2016-05-20 21:00:38 -0700

    Can click on the board or the numbers.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 $(function() {
     // @todo: Must save state to local storage
-    // @todo: click on the board itself
     // @todo: responsive for horizontal after SCSS
     // @todo: figure out why it's slow and why there is no top padding
     // @todo: add transitions or animations to smooth it all out
@@ -13,10 +12,7 @@ $(function() {
 
     for (i = 21; i > 0; i -= 1) {
         li = $('<li>')
-            .data('open', true)
-            //.data('sector', i)
-            .click(number_discovered)
-            .text(' ' + (i === 21 ? 'Bull' : i) + ' ');
+            .text((i === 21 ? 'Bull' : i));
 
         numbers_list.append(li);
 
@@ -25,9 +21,15 @@ $(function() {
             .setSectorSVG(find_on_dartboard(i));
     }
 
-    $('body')
-        .append($('<h2>').text('Open Numbers'))
-        .append(numbers_list);
+
+    // set observers
+    dartboard.click(click_sector);
+    numbers_list.click(click_sector);
+
+    $('body').append(
+        $('<h2>').text('Open Numbers'),
+        numbers_list
+    );
 
     //console.log(sectors);
 
@@ -40,40 +42,21 @@ $(function() {
     //    });
 
 
-
-
-
-    function number_discovered() {
-        var li = $(this),
-            sector = li.data('sector');
-
-        if (li.data('open')) {
-            li
-                .data('open', false)
-                .addClass('closed');
-            update_sector(sector, false);
-        } else {
-            li
-                .data('open', true)
-                .removeClass('closed');
-            update_sector(sector, true);
-        }
-    }
-
-
     /**
-     * Finds the sector on the board SVG and colors it accordingly.
+     * Event handler for the board and list of numbers
      *
-     * @param sector
-     * @param open
+     * @param e Event
      */
-    function update_sector(sector, open) {
-        var el = find_on_dartboard(sector);
+    function click_sector(e) {
+        var el = $(e.target),
+            sector = false;
 
-        if (open) {
-            el.removeClass('closed');
-        } else {
-            el.addClass('closed');
+        if (!(sector = el.data('sector'))) {
+            sector = el.parent().data('sector');
+        }
+
+        if (sector && sectors[sector]) {
+            sectors[sector].toggleOpen();
         }
     }
 
