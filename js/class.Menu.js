bfbe091e6a6ea092f2f2695cad0fe80ad70d9326
Matt Rossetta <archanglmr@yahoo.com>
2016-05-22 15:17:01 -0700

    Made a much better game menu.

---
+++ js/class.Menu.js
@@ -0,0 +1,46 @@
+/**
+ * Game Menu class
+ *
+ * @constructor
+ */
+function Menu(title) {
+    "use strict";
+
+    var el = $('<section>').addClass('menu');
+
+
+    // PUBLIC METHODS
+    this.show = function() {
+        el.addClass('visible');
+        return this;
+    };
+    this.hide = function() {
+        el.removeClass('visible');
+        return this;
+    };
+    this.addButton = function(title, callback) {
+        var button = $('<button>');
+
+        button.text(title);
+        if (callback) {
+            button.click(callback);
+        }
+
+        el.append(button);
+        return button;
+    };
+
+    this.getElement = function() {
+        return el;
+    };
+
+
+    // CONSTRUCTOR SETUP
+
+    if (title) {
+        el.append($('<h2>').text(title));
+    }
+
+
+    // PRIVATE METHODS
+}
